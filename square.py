fcc5692d1fd5608cd5a3e8d3c11935fbbc449236
Amalcxc <63609692+Amalcxc@users.noreply.github.com>
2020-04-13 22:43:45 +0300

    Add files via upload

---
+++ square.py
@@ -0,0 +1,17 @@
+import turtle  
+skk = turtle.Turtle() 
+  
+for i in range(4): 
+    skk.forward(50) 
+    skk.right(90) 
+      
+turtle.done() 
+
+
+star = turtle.Turtle() 
+  
+for i in range(50): 
+    star.forward(50) 
+    star.right(144) 
+      
+turtle.done() 
